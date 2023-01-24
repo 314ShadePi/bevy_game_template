29f24c47c765e5d8d99a93f0dc92e26d3cc59440
314ShadePi <erikkirkegaardstage@gmail.com>
2023-01-24 13:45:51 +0100

    Structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod components;
+mod entities;
+mod plugins;
+mod resources;
+mod systems;
+
+use crate::plugins::example_plugin::ExamplePlugin;
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ExamplePlugin)
+        .run()
 }
---
+++ src/plugins/example_plugin/mod.rs
@@ -0,0 +1,14 @@
+mod components;
+mod entities;
+mod resources;
+mod systems;
+
+use bevy::prelude::*;
+
+pub struct ExamplePlugin;
+
+impl Plugin for ExamplePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(systems::hello::hello);
+    }
+}
